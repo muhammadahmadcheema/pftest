30ace7bf4883e68053e4eb34521cc8a7d08b57e2
Muhammad Ahmad Cheema <85633115+muhammadahmadcheema@users.noreply.github.com>
2024-09-14 16:27:34 +0500

    Add files via upload

---
+++ truck.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+main()
+{
+
+
+
+cout << "  __________________________________" << endl;
+cout << "  |                                 |" << endl;
+cout << "  |                                 |______" << endl;
+cout << "  |                                 ||     \\" << endl;
+cout << "  |                                 ||      \\" << endl;
+cout << "  |                                 ||       \\" << endl;
+cout << "  |                                 ||--------|)" << endl;
+cout << "  |                                 ||        |" << endl;
+cout << " [|_________________________________||________|]" << endl;
+cout << "   (     )                  (      ) " << endl;
+cout << "   (_____)                  (______)" << endl;
+    
+
+
+
+
+
+
+}
